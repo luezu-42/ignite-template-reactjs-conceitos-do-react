9980c53dd6ad1b60dd9fcfc903515f07212fa287
luezu-42 <leonardouezu@gmail.com>
2022-02-12 15:29:09 -0300

    Adiciona função de adicionar tarefa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,12 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    setTasks([...tasks, task]);
   }
 
   function handleToggleTaskCompletion(id: number) {
